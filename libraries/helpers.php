71937ce5112f323da7433b3b00f350fc5c5e27a8
Nikita <gnvenom@mail.ru>
2022-11-08 23:18:35 +0600

    fixes v6

--- libraries/helpers.php
+++ libraries/helpers.php
@@ -3,7 +3,7 @@
 function array_group($array, $groupKey, $groupValue)
 {
     foreach ($array as $groupItem) {
-        $arrayGroup[$groupItem["{$groupKey}"]][] = $groupItem["{$groupValue}"];
+        $arrayGroup[$groupItem[$groupKey]][] = $groupItem[$groupValue];
     }
 
     return $arrayGroup;
