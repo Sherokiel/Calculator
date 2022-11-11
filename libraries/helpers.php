df5a9e10b3b165a43c56516a5b4aac2315ef03b3
Nikita <gnvenom@mail.ru>
2022-11-11 12:55:19 +0600

    v1.13.01

--- libraries/helpers.php
+++ libraries/helpers.php
@@ -10,3 +10,40 @@ function array_group($array, $key)
 
     return $result;
 }
+
+function welcome($msg1,$msg2)
+{
+    $length = 65;
+    $string = 0;
+    $maxStrings = 14;
+
+    while ($string<=$maxStrings) {
+
+        if ($string < 1 || $string > $maxStrings - 1) {
+            $i = 0;
+
+            while ($i < $length) {
+                echo "*";
+                $i++;
+            }
+            echo PHP_EOL;
+
+        } else {
+            switch ($string)
+            {
+                case ($string == round($maxStrings / 3)):
+                    $msg = $msg1;
+                    break;
+                case ($string == round($maxStrings / 2)):
+                    $msg = $msg2;
+                    break;
+                default:
+                    $msg = '';
+            }
+            echo "*";
+            echo str_pad($msg, $length - 2, ' ', STR_PAD_BOTH);
+            echo "*" . PHP_EOL;
+        }
+        $string++;
+    }
+}
