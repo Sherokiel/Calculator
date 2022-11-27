37547a600e43954a47396d8d824d2549f073c3c8
Nikita <gnvenom@mail.ru>
2022-11-27 12:51:46 +0600

    v1.21.10

--- app/Repositories/HistoryRepository.php
+++ app/Repositories/HistoryRepository.php
@@ -12,7 +12,6 @@ class HistoryRepository extends JsonBaseRepository
     public function createDataItem($dataContent)
     {
         $dataContent = array_slice($dataContent, 0, 5);
-
         $dataContentValid = array_key_exists('date', $dataContent) && array_key_exists('first_operand', $dataContent) && array_key_exists('second_operand', $dataContent) && array_key_exists('sign', $dataContent) && array_key_exists('result', $dataContent);
 
         if (!$dataContentValid) {
