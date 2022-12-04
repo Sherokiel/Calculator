1c8866d3f2b3c5be4d61b7ec79e10f4b0eb09012
Nikita <gnvenom@mail.ru>
2022-12-05 00:27:19 +0600

    v1.23.07

--- app/HistoryConsoleExporter.php
+++ app/HistoryConsoleExporter.php
@@ -4,13 +4,6 @@ namespace App\Exporters;
 
 class HistoryConsoleExporter extends BaseHistoryExporter
 {
-    protected $historyRepository;
-
-    public function __construct()
-    {
-        return parent::__construct();
-    }
-
     protected function output($historyLine)
     {
         return info($historyLine, 1);
